1670819d1ee38d410722c9df72bd195485826241
zhengzhiguang <zhengzhiguang@trunk.tech>
2024-12-31 18:22:30 +0800

    <fix>(target_fusion):优化融合中目标存在性逻辑

--- trunk_perception/app/target_fusion/data_fusion/src/existence_fusion_1l1r1v.cpp
+++ trunk_perception/app/target_fusion/data_fusion/src/existence_fusion_1l1r1v.cpp
@@ -53,6 +53,18 @@ float ExistenceFusion1L1R1V::Compute(const FusedObject::Ptr& fused_object_ptr) {
 				(front_vision_consecutive_hit < 30)) {
 			score = 0.3;
 		}
+
+		// 2024-12-30 解决近处（盲区）无激光测量或激光测量不稳定 VTI-14302
+		if (front_radar_consecutive_hit >= 1 && front_vision_consecutive_hit >= 1 ) {
+			Eigen::Matrix4d local_to_car = fused_object_ptr->odo_lidar_ptr->Matrix().inverse();
+			Eigen::Vector4d center_vec(fused_object_ptr->center.x(), fused_object_ptr->center.y(), fused_object_ptr->center.z(), 1.0);
+			center_vec = local_to_car * center_vec;
+			Eigen::Vector3d temp_center = Eigen::Vector3d(center_vec.x(), center_vec.y(), center_vec.z());
+			if ( std::fabs(temp_center.x()) < 20 && std::fabs(temp_center.y()) < 2.0 ) {
+				score = 0.3;
+			}
+		}
+
   } else if (lidar_consecutive_hit <= 1) {
 		score = 0.3;
 		if ((front_radar_consecutive_hit >= 1) || (front_vision_consecutive_hit >= 1)) {
@@ -71,15 +83,15 @@ float ExistenceFusion1L1R1V::Compute(const FusedObject::Ptr& fused_object_ptr) {
     score = 0.3;
   }
 
-	// @author zzg 2024_12_25 增加逻辑：目标类型为 TRUCK、VEHICLE，有过激光连续命中 15 帧以上,，且毫米波一直连续命中 25 帧以上，，则维持融合目标
+	// @author zzg 2024_12_25 增加逻辑：目标类型为 TRUCK、VEHICLE， 激光雷达总生命周期 35 帧以上，且 毫米波一直连续命中 25 帧以上，则维持融合目标
 	// 为了解决近距离怼脸 TRUCK、VEHICLE 由于 激光连续丢失多帧 导致的目标丢失；   解决特定问题
-	if ( (score < 0.3) && (fused_object_ptr->front_radar_consecutive_hit > 25) && (fused_object_ptr->lidar_consecutive_hit_his > 15)) {
+	if ( (score < 0.3) && (fused_object_ptr->front_radar_consecutive_hit > 25) && (fused_object_ptr->lidar_total_life > 35)) {
 		if (fused_object_ptr->type == ObjectType::TRUCK || fused_object_ptr->type == ObjectType::VEHICLE) {
 			Eigen::Matrix4d local_to_car = fused_object_ptr->odo_lidar_ptr->Matrix().inverse();
 			Eigen::Vector4d center_vec(fused_object_ptr->center.x(), fused_object_ptr->center.y(), fused_object_ptr->center.z(), 1.0);
 			center_vec = local_to_car * center_vec;
 			Eigen::Vector3d temp_center = Eigen::Vector3d(center_vec.x(), center_vec.y(), center_vec.z());
-			if (temp_center.x() < 25 && std::fabs(temp_center.y()) < 2.0 ) {
+			if (std::fabs(temp_center.x()) < 25 && std::fabs(temp_center.y()) < 2.5 ) {
 				score = 0.3;
 			}
 		}
